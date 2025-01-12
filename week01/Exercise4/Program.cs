dbc87cadf1b7af90154659d723ba88c88d0e5f7a
littlemissmiso <todayisdelightful@gmail.com>
2025-01-11 19:06:50 -0700

    List (Max/Avg/Count) practice

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,50 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int> numberList = new List<int>();
+
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+            Console.Write("Enter number: ");
+            string numberString = Console.ReadLine();
+            number = int.Parse(numberString);
+
+            if (number != 0)
+            {
+                numberList.Add(number);
+            }   
+        }
+        
+        int sum = 0;
+        foreach (int num in numberList)
+        {
+            sum += num;
+           
+        }
+
+        Console.WriteLine($"The sum is: {sum}"); 
+
+        float average = ((float)sum) / numberList.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int maxNumber = numberList[0];
+
+        foreach (int num in numberList)
+        {
+            if (num > maxNumber)
+            {
+                maxNumber = num;
+            }
+        }
+        Console.WriteLine($"The max is: {maxNumber}");
+    
     }
 }
