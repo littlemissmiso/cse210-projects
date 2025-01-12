c94dd46d823400a0b1324b99e5874f3d1892c31b
littlemissmiso <todayisdelightful@gmail.com>
2025-01-12 16:06:34 -0700

    Function Practice

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        // Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        DisplayWelcomeMessage();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int userSquareNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, userSquareNumber);    
+    }   
+        
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!"); 
     }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int squareNum = number * number;
+
+        return squareNum;
+    }
+
+    static void DisplayResult(string userName, int squareNum)
+    {
+        Console.WriteLine($"Thanks {userName}, the square root of your number is {squareNum}.");
+    }
+
+        
 }
